cafb1a2d2ee3c92f68fa5aff5b6ef93cf1275725
Anton Dzodzikov <anton.dzodzikov@gmail.com>
2017-02-24 17:15:50 +1000

    Optimize imports

--- storage-fs/src/test/groovy/solutions/lightprocessing/storage/fs/reader/OperationTypeReaderTest.groovy
+++ storage-fs/src/test/groovy/solutions/lightprocessing/storage/fs/reader/OperationTypeReaderTest.groovy
@@ -3,10 +3,11 @@ package solutions.lightprocessing.storage.fs.reader
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests
-import org.testng.Assert
 import org.testng.annotations.Test
 import solutions.lightprocessing.config.StorageFsTestConfiguration
 import solutions.lightprocessing.storage.fs.domain.OperationType
+
+import static org.testng.Assert.assertEquals
 
 @ContextConfiguration(classes = StorageFsTestConfiguration.class)
 class OperationTypeReaderTest extends AbstractTestNGSpringContextTests {
@@ -17,7 +18,7 @@ class OperationTypeReaderTest extends AbstractTestNGSpringContextTests {
     void readsTypesFromFilesystem() {
         Map<String, OperationType> loadedTypes = operationTypeReader.readTypesFromFilesystem()
 
-        Assert.assertEquals(loadedTypes.size(), 2)
-        Assert.assertEquals(loadedTypes.keySet(), ["SAY_HELLO", "RETURN_NUMBER_SEQUENCE"].toSet())
+        assertEquals(loadedTypes.size(), 2)
+        assertEquals(loadedTypes.keySet(), ["SAY_HELLO", "RETURN_NUMBER_SEQUENCE"].toSet())
     }
 }
